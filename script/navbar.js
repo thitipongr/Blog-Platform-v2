ac5ffee7d3bbdc4448e69c26a49c450598614b02
thitipongr <thitipong.r.psn@gmail.com>
2023-10-16 02:26:20 +0700

    chang theme and add transaction

--- script/navbar.js
+++ script/navbar.js
@@ -59,3 +59,34 @@ fetch("../mock/statistical-data.json")
   })
   .catch((err) => console.log("error:", err));
 
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-light");
+    document.getElementById("moon").style.display = "none";
+    document.getElementById("sun").style.display = "flex";
+  } else {
+    setTheme("theme-dark");
+    document.getElementById("moon").style.display = "flex";
+    document.getElementById("sun").style.display = "none";
+  }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-dark");
+    document.getElementById("moon").style.display = "flex";
+    document.getElementById("sun").style.display = "none";
+  } else {
+    setTheme("theme-light");
+    document.getElementById("moon").style.display = "none";
+    document.getElementById("sun").style.display = "flex";
+  }
+})();
