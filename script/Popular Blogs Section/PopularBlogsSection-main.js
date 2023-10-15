6f7fea7287c8b595c84e124e36c5f6f07d602f5c
thitipongr <thitipong.r.psn@gmail.com>
2023-10-15 14:59:17 +0700

    popular blog mobile view

--- script/Popular Blogs Section/PopularBlogsSection-main.js
+++ script/Popular Blogs Section/PopularBlogsSection-main.js
@@ -1,70 +1,43 @@
 fetch("mock/blog-data.json")
   .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log("error"));
-
-const headLine = "Lorem ipsum dolor sit amet consectetur";
-const blogName = "Lorem ipsum dolor sit amet consectetur";
-const content =
-  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, quidem non quam exercitationem odio autem voluptas voluptatem incidunt laboriosam facilis";
-
-const setHtml = `
-<div class="container">
-  <div class="hero-con">
-    <h1>${headLine}</h1>
-    <div class="hero-img">
-      <img
-        src="https://images.pexels.com/photos/1067333/pexels-photo-1067333.jpeg?auto=compress&cs=tinysrgb&w=2560"
-        alt="hero-img"
-      />
-    </div>
-    <div class="hero-info-con">
-      <div class="hero-info-article">
-        <h3>${blogName}</h3>
+  .then((data) => data.blogs.sort(compareByView))
+  .then((sortedData) => sortedData.slice(0, 10))
+  .then((popularBlogs) => {
+    let setHtml = "";
+    popularBlogs.map((popularBlogs_data) => {
+      console.log(popularBlogs_data);
+      setHtml += `<div class="popular-blogs-list-item">
+      <div class="img">
+        <img
+          src="${popularBlogs_data.blog_cove}?auto=compress&cs=tinysrgb&w=2560"
+          alt="popular-blog-list-img"
+        />
+      </div>
+      <div class="title">
+        <h3>${popularBlogs_data.title}</h3>
         <i class="fa-solid fa-up-right-from-square"></i>
       </div>
-      <div class="hero-info-detail-con">
-        <div class="hero-info-detail-maindetail-con">
-          <div class="hero-info-detail-maindetail">
-            <p>
-              ${content}
-            </p>
-          </div>
-        </div>
-        <div class="hero-info-detail-subdetail-con">
-          <div class="hero-info-writer-con">
-            <div class="hero-info-writer-detail-con">
-              <div class="hero-info-writer-detail-label">
-                Written by
-              </div>
-              <div class="hero-info-writer-detail-info-con">
-                <div class="hero-info-writer-detail-info-pic">
-                  <img
-                    src="https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&w=25"
-                    alt="hero-writer-img"
-                  />
-                </div>
-                <div class="hero-info-writer-detail-info-name">
-                  Lorem ipsum
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="hero-info-publihed-con">
-            <div class="hero-info-publihed-lable">Published on</div>
-            <div class="hero-info-publihed-date">09 October 2023</div>
-          </div>
-        </div>
-        <div class="hero-info-detail-contegory-con">
-          <div class="hero-info-contegory-label">Contegory</div>
-          <div class="hero-info-contegory-info-con">
-            <div class="hero-info-contegory-info">Lorem</div>
-            <div class="hero-info-contegory-info">Ipsum</div>
-            <div class="hero-info-contegory-info">Dolor</div>
-          </div>
-        </div>
+      <div class="content">
+        <p>
+        ${popularBlogs_data.content}
+        </p>
       </div>
-    </div>
-  </div>
-</div>`;
-document.getElementById("section-hero").innerHTML = setHtml;
+      <div class="category-view">
+        <div class="category-list">
+          <div class="category-item">${popularBlogs_data.category}</div>
+        </div>
+        <div class="view">${popularBlogs_data.views} views</div>
+      </div>
+    </div>`;
+    });
+    return setHtml;
+  })
+  .then(
+    (setHtml) =>
+      (document.getElementById("popular-blogs-list").innerHTML += setHtml)
+  )
+  .catch((err) => console.log("error:", err));
+
+function compareByView(a, b) {
+  return b.views - a.views;
+}
